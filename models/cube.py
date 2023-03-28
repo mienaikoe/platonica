3618f39e4402150d6bd767127c27484d37ac70d0
AQW <alice@valour.me>
2023-03-27 21:32:10 -0400

    tetra

--- models/cube.py
+++ models/cube.py
@@ -1,11 +1,6 @@
 from models.model import Model
 import numpy as np
-
-
-def triangle_vertices_from_indices(vertices, indices):
-    triangle_vertices = [vertices[ind] for triangle in indices for ind in triangle]
-    return np.array(triangle_vertices, dtype="f4")
-
+from models.helpers import triangle_vertices_from_indices
 
 cube_corners = [
     (-1, -1, 1),
