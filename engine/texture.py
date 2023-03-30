e0904a64246d72a381a29f64811d560f1165c130
AQW <alice@valour.me>
2023-03-29 22:06:48 -0400

    maze texture

--- engine/texture.py
+++ engine/texture.py
@@ -9,3 +9,22 @@ def get_texture(ctx: moderngl.Context, path: str):
         components=3,
         data=pygame.image.tostring(texture, 'RGB'))
     return texture
+
+texture_maps = {
+    'tetra_tex_test.png': {
+        'uv': [
+            (0.504, 0.953), #0
+            (0.262, 0.496), #1
+            (0.744, 0.496), #2
+            (0.02, 0.142), #3
+            (0.504, 0.142), #4
+            (0.986, 0.142), #5
+        ],
+        'faces': [
+            (1, 0, 2),
+            (1, 4, 3),
+            (4, 1, 2),
+            (2, 5, 4),
+        ]
+    }
+}
--- models/tetra.py
+++ models/tetra.py
@@ -4,7 +4,7 @@ import numpy as np
 from engine.camera import Camera
 from models.model import Model
 from models.helpers import triangle_vertices_from_indices
-from engine.texture import get_texture
+from engine.texture import get_texture, texture_maps
 
 
 corners = [
@@ -23,24 +23,13 @@ face_indices = [
 
 geom_vertices = triangle_vertices_from_indices(corners, face_indices)
 
-
-texture_corners = [
-    (0.5, 0.2), # orange
-    (0.0, 1.0), # green
-    (1.0, 1.0), # yellow
-]
-texture_indices = [
-    (0, 1, 2),
-    (0, 1, 2),
-    (0, 1, 2),
-    (0, 1, 2),
-]
-texture_vertices = triangle_vertices_from_indices(texture_corners, texture_indices)
-
 class Tetrahedron(Model):
     def __init__(self, ctx: moderngl.Context, camera: Camera):
-        texture = get_texture(ctx, 'assets/textures/texture_test.png')
+        self.texture_file = 'tetra_tex_test.png'
+        texture = get_texture(ctx, 'assets/textures/' + self.texture_file)
         super().__init__(ctx, camera, texture)
 
     def get_vertex_data(self):
+        tex_map = texture_maps[self.texture_file]
+        texture_vertices = triangle_vertices_from_indices(tex_map['uv'], tex_map['faces'])
         return np.hstack([texture_vertices, geom_vertices])
