33c963b7fba94ab931d4f3d1e6d73f5fb9361ea0
Jesse Lee <mienaikoe@gmail.com>
2023-04-15 11:05:37 -0400

    soundtrack fixes

--- game/engine/audio/soundtrack.py
+++ game/engine/audio/soundtrack.py
@@ -29,16 +29,13 @@ class Soundtrack:
     self.tracks = None
     self.track_num = 0
     mixer.music.set_endevent(MUSIC_TRACK_END)
-    print("init new soundtrack")
 
   def handle_event(self, event: pygame.event.Event, world_time: int):
     if event.type == MUSIC_TRACK_END:
-      print("Music Track End", self.track_num)
       mixer.music.queue(self.tracks[self.track_num])
 
   def set_song(self, song_name: SoundtrackSong):
     self.tracks = TRACK_ATLAS[song_name]
-    print("set song", self.tracks)
     mixer.music.load(self.tracks[self.track_num])
 
   def set_volume(self, vol: float):
@@ -49,9 +46,9 @@ class Soundtrack:
     mixer.music.queue(self.tracks[self.track_num])
 
   def advance(self):
-    print("advancing")
+    if self.track_num >= len(self.tracks) - 1:
+      return
     self.track_num += 1
-    print(self.tracks[self.track_num])
     mixer.music.queue(self.tracks[self.track_num])
     if self.track_num >= len(self.tracks) - 1:
       mixer.music.set_endevent() # stops event firing
